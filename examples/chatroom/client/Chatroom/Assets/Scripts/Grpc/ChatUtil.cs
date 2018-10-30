a22bea76eb310fc8138e9c0eca82108d51d78d71
armariya <arm.armariya@gmail.com>
2018-10-30 17:35:07 +0700

    Add Chatroom Unity3d example

---
+++ examples/chatroom/client/Chatroom/Assets/Scripts/Grpc/ChatUtil.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Chat {
+    public static class ChatUtil {
+        public static bool Exists(this LoginResponse response) {
+            return response != null && response.Token.Length != 0;
+        }
+
+        public static bool Exists(this LogoutResponse response) {
+            return response != null;
+        }
+    }
+}
