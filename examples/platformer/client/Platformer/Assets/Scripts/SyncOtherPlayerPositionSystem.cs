6befb226f3b664b0d2bae9360bbed828324a5dc2
armariya <arm.armariya@gmail.com>
2018-11-28 12:44:23 +0700

    Increase performance
    Change from ComponentSystem to JobComponentSystem

--- examples/platformer/client/Platformer/Assets/Scripts/SyncOtherPlayerPositionSystem.cs
+++ examples/platformer/client/Platformer/Assets/Scripts/SyncOtherPlayerPositionSystem.cs
@@ -1,37 +1,42 @@
 ﻿using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Jobs;
 
 namespace Platformer {
-    public class SyncOtherPlayerPositionSystem : ComponentSystem {
-
-        struct OtherPlayer {
+    public class SyncOtherPlayerPositionSystem : JobComponentSystem {
+        struct Data {
             public readonly int Length;
             public ComponentDataArray<Position> Positions;
             public ComponentDataArray<OtherPlayerData> OtherPlayerDatas;
+
         }
 
-        [Inject] private OtherPlayer _otherPlayer;
+        [Inject] private Data _data;
 
-        protected override void OnUpdate() {
-            if (PlatformerGrpcController.Instance.PlayerPositionByIdResponses.Length == 0) {
-                return;
-            }
+        private struct SyncOtherPlayerPositionJob : IJobParallelFor {
+            public Data Data;
+           
+            public void Execute(int index) {
+                if (PlatformerGrpcController.Instance.PlayerPositionByIdResponses.Length == 0) {
+                    return;
+                }
 
-            for (int i = 0; i < _otherPlayer.Length; i++) {
-                int id = _otherPlayer.OtherPlayerDatas[i].ID;
-
+                int id = Data.OtherPlayerDatas[index].ID;
                 if (PlatformerGrpcController.Instance.PlayerPositionByIdResponses.TryGetValue(id, out float3 position)) {
-                    _otherPlayer.Positions[i] = new Position {
+                    Data.Positions[index] = new Position {
                         Value = position
                     };
 
                     PlatformerGrpcController.Instance.PlayerPositionByIdResponses.Remove(id);
                 }
-                else {
-                    continue;
-                }
             }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps) {
+            return new SyncOtherPlayerPositionJob {
+                Data = _data
+            }.Schedule(_data.Length, 64, inputDeps);
         }
     }
 }
